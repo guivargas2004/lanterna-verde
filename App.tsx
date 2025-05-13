5e7a9844d70deb1d5c5e410e91a97526fc84b55b
Guilherme Faustino Vargas <gfvargas2004@gmail.com>
2025-05-13 17:28:41 -0300

    Primeiro projeto React Native

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity  } from 'react-native';
+import SymbolOn from './assets/imgs/symbol-on.png';
+import SymbolOff from './assets/imgs/symbol-off.png'
 
 export default function App() {
+  const [isActive, setisActive] = useState(false)
+
+  function handleSymbol (){
+    setisActive((oldValue:boolean) => {
+      return !oldValue
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol}>
+      <Image 
+       source={isActive ? SymbolOn : SymbolOff}
+       />
+       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  containerOff: {
+  flex: 1,
+  backgroundColor: 'white',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  }
 });
